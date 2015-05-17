45f3184497120ad348a646a797b80173c5089c58
Eric Lopez <elopez@tricksters.net>
2015-05-17 11:02:03 -0700

    Fixed service and config looping

---
+++ autoconfig/roles/jumphost/templates/home/vmware/admin-openrc.sh
@@ -0,0 +1,7 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME={{ tenant_admin }}
+export OS_TENANT_NAME={{ tenant_admin }}
+export OS_USERNAME={{ tenant_admin_user }}
+export OS_PASSWORD={{ tenant_admin_password }}
+export OS_AUTH_URL=http://controller:35357/v3
---
+++ autoconfig/roles/jumphost/templates/home/vmware/demo-openrc.sh
@@ -0,0 +1,7 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME={{ tenant_demo }}
+export OS_TENANT_NAME={{ tenant_demo }}
+export OS_USERNAME={{ tenant_demo_user }}
+export OS_PASSWORD={{ tenant_demo_password }}
+export OS_AUTH_URL=http://controller:5000/v3
